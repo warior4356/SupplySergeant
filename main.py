fb7be5cc7634461f310a38a6cfc86a3a0c294221
warior4356 <warior4356@yahoo.com>
2024-10-24 00:20:44 -0700

    Adds target quantities

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ except:
 import cfg
 import items
 import ships
+import esipy
 from esipy import EsiApp
 from esipy import EsiClient
 from esipy import EsiSecurity
@@ -108,29 +109,29 @@ def get_item_ids():
     types.close()
 
 
-def generate_report(file_name, station_ids, ship_list, charge_list, sheet_index, corporation_id, region_id):
+def generate_report(file_name, station_ids, ship_list, item_list, sheet_index, corporation_id, region_id, contracts=True):
     staging_ships = dict()  # [match, hull match, parts, hull_id, max fittable from market]
-    staging_charges = dict()  # [name, local count, local price,  jita volume, jita price]
-    staging_parts = dict()  # [name, local count, local price, jita volume, jita price]
+    staging_charges = dict()  # [name, local count, local price,  jita volume, jita price, target quantity]
+    staging_parts = dict()  # [name, local count, local price, jita volume, jita price, target quantity]
     contract_owners = dict()
 
     # Fetch items
-    for item in charge_list:
-        staging_charges[int(item_ids[item])] = [item, 0, 0, 0, 0]
+    for item in item_list:
+        staging_charges[int(item_ids[item[0]])] = [item[0], 0, 0, 0, 0, item[1]]
 
     # Parse ships
-    for ship_name in ship_list:
-        fitting = open("ships/" + ship_name + ".txt", 'r', encoding='utf-8')
-        ship_name = ship_name.strip("[]")
+    for ship in ship_list:
+        fitting = open("ships/" + ship[0] + ".txt", 'r', encoding='utf-8')
+        ship_name = ship[0].strip("[]")
         lines = fitting.readlines()
         ship_id = int(item_ids[ship_name.split(",")[0]])
-        staging_ships[ship_name] = [0, 0, {ship_id: 1}, ship_id, -1]
-        staging_parts[int(item_ids[ship_name.split(",")[0]])] = [ship_name.split(",")[0], 0, 0, 0, 0]
+        staging_ships[ship_name] = [0, 0, {ship_id: 1}, ship_id, -1, ship[1]]
+        staging_parts[int(item_ids[ship_name.split(",")[0]])] = [ship_name.split(",")[0], 0, 0, 0, 0, 0]
         for line in lines[1:]:
             if line.strip() in item_ids.keys():
                 module_id = int(item_ids[line.strip()])
                 staging_ships[ship_name][2][module_id] = staging_ships[ship_name][2].get(module_id, 0) + 1
-                staging_parts[module_id] = [line.strip(), 0, 0, 0, 0]
+                staging_parts[module_id] = [line.strip(), 0, 0, 0, 0, 0]
             elif line.strip().rsplit(',', 1)[0] in item_ids.keys():
                 parts = line.strip().rsplit(',', 1)
                 module_id = int(item_ids[parts[0]])
@@ -138,13 +139,13 @@ def generate_report(file_name, station_ids, ship_list, charge_list, sheet_index,
                 # print(charge_id)
                 staging_ships[ship_name][2][module_id] = staging_ships[ship_name][2].get(module_id, 0) + 1
                 # staging_ships[ship_name][2][charge_id] = staging_ships[ship_name][2].get(charge_id, 0) + 1
-                staging_parts[module_id] = [parts[0], 0, 0, 0, 0]
+                staging_parts[module_id] = [parts[0], 0, 0, 0, 0, 0]
                 # staging_charges[charge_id] = [parts[1].lstrip(' '), 0, 0, 0, 0]
             elif line.strip().rsplit(' ', 1)[0] in item_ids.keys():
                 item_name = line.strip().rsplit(' ', 1)[0]
                 count = line.strip().rsplit(' ', 1)[1].strip("x")
                 charge_id = int(item_ids[item_name])
-                staging_charges[charge_id] = [item_name, 0, 0, 0, 0]
+                staging_charges[charge_id] = [item_name, 0, 0, 0, 0, 0]
                 staging_ships[ship_name][2][charge_id] = staging_ships[ship_name][2].get(charge_id, 0) + int(count)
         fitting.close()
     # print(staging_parts)
@@ -231,124 +232,6 @@ def generate_report(file_name, station_ids, ship_list, charge_list, sheet_index,
                 if current_price == 0 or current_price > result.get("price"):
                     staging_parts[result.get("type_id")][4] = result.get("price")
 
-    # print(staging_parts)
-
-    # Get corp/alliance contracts with ships
-    contract_results = []
-
-    op = app.op['get_corporations_corporation_id_contracts'](
-        corporation_id=corporation_id,
-        page=1,
-    )
-
-    res = esi_client.head(op)
-
-    if res.status == 200:
-        number_of_pages = res.header['X-Pages'][0]
-
-        # now we know how many pages we want, let's prepare all the requests
-        operations = []
-        for page in range(1, number_of_pages+1):
-            operations.append(
-                app.op['get_corporations_corporation_id_contracts'](
-                    corporation_id=corporation_id,
-                    page=page,
-                )
-            )
-
-        contract_results = esi_client.multi_request(operations)
-
-    # print(contract_results)
-    for pair in contract_results:
-        for result in pair[1].data:
-            if result.get("status") == "outstanding" and result.get("type") == "item_exchange" \
-                    and result.get("start_location_id") in station_ids and \
-                    _convert_swagger_dt(result.get("date_expired")) > datetime.datetime.utcnow():
-                sleep(.5)
-                op = app.op['get_corporations_corporation_id_contracts_contract_id_items'](
-                    contract_id=result.get("contract_id"),
-                    corporation_id=corporation_id,
-                )
-                # print(result)
-
-                contract_owners[result.get("contract_id")] = [result.get("title"), result.get("issuer_id")]
-
-                while(True):
-                    try:
-                        contents = esi_client.request(op)
-                    except:
-                        sleep(3)
-                        continue
-                    break
-
-                contract_types = list()
-
-                for item in contents.data:
-                    contract_types.append(item.get("type_id"))
-
-                for ship in staging_ships.keys():
-                    if all(item in contract_types for item in staging_ships[ship][2].keys()):
-                        staging_ships[ship][0] += 1
-                    elif staging_ships[ship][3] in contract_types:
-                        staging_ships[ship][1] += 1
-
-    # Get public contracts with ships
-    contract_results = []
-
-    op = app.op['get_contracts_public_region_id'](
-        region_id=region_id,
-        page=1,
-    )
-
-    res = esi_client.head(op)
-
-    if res.status == 200:
-        number_of_pages = res.header['X-Pages'][0]
-
-        # now we know how many pages we want, let's prepare all the requests
-        operations = []
-        for page in range(1, number_of_pages + 1):
-            operations.append(
-                app.op['get_contracts_public_region_id'](
-                    region_id=region_id,
-                    page=page,
-                )
-            )
-
-        contract_results = esi_client.multi_request(operations)
-
-    # print(contract_results)
-    for pair in contract_results:
-        for result in pair[1].data:
-            if result.get("type") == "item_exchange" \
-                    and result.get("start_location_id") in station_ids and \
-                    _convert_swagger_dt(result.get("date_expired")) > datetime.datetime.utcnow():
-                sleep(.5)
-                op = app.op['get_contracts_public_items_contract_id'](
-                    contract_id=result.get("contract_id"),
-                )
-                # print(result)
-
-                contract_owners[result.get("contract_id")] = [result.get("title"), result.get("issuer_id")]
-
-                while (True):
-                    try:
-                        contents = esi_client.request(op)
-                    except:
-                        sleep(3)
-                        continue
-                    break
-
-                contract_types = list()
-
-                for item in contents.data:
-                    contract_types.append(item.get("type_id"))
-
-                for ship in staging_ships.keys():
-                    if all(item in contract_types for item in staging_ships[ship][2].keys()):
-                        staging_ships[ship][0] += 1
-                    elif staging_ships[ship][3] in contract_types:
-                        staging_ships[ship][1] += 1
 
     # Determine how many ships can be fit from what's on market
     for ship in staging_ships.keys():
@@ -364,9 +247,135 @@ def generate_report(file_name, station_ids, ship_list, charge_list, sheet_index,
             else:
                 staging_ships[ship][4] = 0
 
-            # print(staging_ships[ship])
+    # Determine target number of modules and charges based on target quantities
+    for ship in staging_ships.keys():
+        for item in staging_ships[ship][2].keys():
+            if item in staging_parts.keys():
+                staging_parts[item][5] = staging_parts[item][5] + (staging_ships[ship][2][item] * staging_ships[ship][5])
+            if item in staging_charges.keys():
+                staging_charges[item][5] = staging_charges[item][5] + (staging_ships[ship][2][item] * staging_ships[ship][5])
 
-    # print(staging_ships)
+    if contracts:
+        # Get corp/alliance contracts with ships
+        contract_results = []
+
+        op = app.op['get_corporations_corporation_id_contracts'](
+            corporation_id=corporation_id,
+            page=1,
+        )
+
+        res = esi_client.head(op)
+
+        if res.status == 200:
+            number_of_pages = res.header['X-Pages'][0]
+
+            # now we know how many pages we want, let's prepare all the requests
+            operations = []
+            for page in range(1, number_of_pages+1):
+                operations.append(
+                    app.op['get_corporations_corporation_id_contracts'](
+                        corporation_id=corporation_id,
+                        page=page,
+                    )
+                )
+
+            contract_results = esi_client.multi_request(operations)
+
+        # print(contract_results)
+        for pair in contract_results:
+            for result in pair[1].data:
+                if result.get("status") == "outstanding" and result.get("type") == "item_exchange" \
+                        and result.get("start_location_id") in station_ids and \
+                        _convert_swagger_dt(result.get("date_expired")) > datetime.datetime.utcnow():
+                    sleep(.5)
+                    op = app.op['get_corporations_corporation_id_contracts_contract_id_items'](
+                        contract_id=result.get("contract_id"),
+                        corporation_id=corporation_id,
+                    )
+                    # print(result)
+
+                    contract_owners[result.get("contract_id")] = [result.get("title"), result.get("issuer_id")]
+
+                    while(True):
+                        try:
+                            contents = esi_client.request(op)
+                        except:
+                            sleep(3)
+                            continue
+                        break
+
+                    contract_types = list()
+
+                    for item in contents.data:
+                        contract_types.append(item.get("type_id"))
+
+                    for ship in staging_ships.keys():
+                        if all(item in contract_types for item in staging_ships[ship][2].keys()):
+                            staging_ships[ship][0] += 1
+                        elif staging_ships[ship][3] in contract_types:
+                            staging_ships[ship][1] += 1
+
+        # Get public contracts with ships
+        contract_results = []
+
+        op = app.op['get_contracts_public_region_id'](
+            region_id=region_id,
+            page=1,
+        )
+
+        res = esi_client.head(op)
+
+        if res.status == 200:
+            number_of_pages = res.header['X-Pages'][0]
+
+            # now we know how many pages we want, let's prepare all the requests
+            operations = []
+            for page in range(1, number_of_pages + 1):
+                operations.append(
+                    app.op['get_contracts_public_region_id'](
+                        region_id=region_id,
+                        page=page,
+                    )
+                )
+
+            contract_results = esi_client.multi_request(operations)
+
+        # print(contract_results)
+        for pair in contract_results:
+            for result in pair[1].data:
+                if result.get("type") == "item_exchange" \
+                        and result.get("start_location_id") in station_ids and \
+                        _convert_swagger_dt(result.get("date_expired")) > datetime.datetime.utcnow():
+                    sleep(.5)
+                    op = app.op['get_contracts_public_items_contract_id'](
+                        contract_id=result.get("contract_id"),
+                    )
+                    # print(result)
+
+                    contract_owners[result.get("contract_id")] = [result.get("title"), result.get("issuer_id")]
+
+                    while (True):
+                        try:
+                            contents = esi_client.request(op)
+                        except:
+                            sleep(3)
+                            continue
+                        break
+
+                    contract_types = list()
+
+                    for item in contents.data:
+                        contract_types.append(item.get("type_id"))
+
+                    for ship in staging_ships.keys():
+                        if all(item in contract_types for item in staging_ships[ship][2].keys()):
+                            staging_ships[ship][0] += 1
+                        elif staging_ships[ship][3] in contract_types:
+                            staging_ships[ship][1] += 1
+
+
+
+        # print(staging_ships)
 
     # Write to outfile and google sheets
     out_file = open("output/" + file_name, 'w')
@@ -404,6 +413,7 @@ def generate_report(file_name, station_ids, ship_list, charge_list, sheet_index,
         cell_list.append(Cell(row=row, col=3, value=int(staging_charges[key][2])))
         cell_list.append(Cell(row=row, col=4, value=int(staging_charges[key][3])))
         cell_list.append(Cell(row=row, col=5, value=int(staging_charges[key][4])))
+        cell_list.append(Cell(row=row, col=7, value=int(staging_charges[key][5])))
         row += 1
 
     sheet.update_cells(cell_list)
@@ -423,6 +433,7 @@ def generate_report(file_name, station_ids, ship_list, charge_list, sheet_index,
         cell_list.append(Cell(row=row, col=3, value=int(staging_parts[key][2])))
         cell_list.append(Cell(row=row, col=4, value=int(staging_parts[key][3])))
         cell_list.append(Cell(row=row, col=5, value=int(staging_parts[key][4])))
+        cell_list.append(Cell(row=row, col=7, value=int(staging_parts[key][5])))
         row += 1
 
     sheet.update_cells(cell_list)
@@ -447,7 +458,8 @@ def main():
     # generate_report("elanoda.csv", [1040246076254], ships.elanoda, items.elanoda, 4, 1018389948, 10000016)
     # generate_report("enaluri.csv", [60015068], ships.enaluri, items.enaluri, 2, 1018389948, 10000069)
     # generate_report("UMI_KK.csv", [1036351551330], ships.preds, items.preds, 0, 1018389948, 10000010)
-    generate_report("GY5_26.csv", [1036684561086], ships.GY5_26, items.GY5_26, 0, 1018389948, 10000035)
+    generate_report("3T7_M8.csv", [1043621617719], ships._3T7_M8, items._3T7_M8, 0, 1018389948, 10000035, contracts=False)
+    # generate_report("F4R2_Q.csv", [1044008398262], ships.F4R2_Q, items.F4R2_Q, 3, 1018389948, 10000014)
     # generate_report("5ZXX_K.csv", [1038708751029, 1039071618828], ships._5ZXX_K, items._5ZXX_K, 4, 1018389948, 10000023)
     # print(check_location(60012580, 30002005))
     # print(check_location(1037022287754, 30002005))
